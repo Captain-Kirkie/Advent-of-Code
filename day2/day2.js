fac1429457ea8337c39f0e4d199c03bea5a4b1d1
kirkH <kirk@passivelogic.com>
2022-12-12 19:28:33 -0700

    Part 2

--- day2/day2.js
+++ day2/day2.js
@@ -10,6 +10,7 @@
  * (0 if you lost, 3 if the round was a draw, and 6 if you won).
  */
 
+// part 2:  X ==> lose, Y ==> draw, Z ==> win
 const { readData } = require("../readData.js");
 
 const actions = {
@@ -17,6 +18,13 @@ const actions = {
     Paper: "paper",
     Scissors: "scissors",
 };
+
+const myVals = {
+    rock: 1,
+    paper: 2,
+    scissors: 3,
+};
+
 // rock 1, 2 paper, 3 scissors
 const response = {
     A: { action: actions.Rock, score: null },
@@ -38,6 +46,12 @@ const result = {
 
 const resultVal = { win: 6, lose: 0, draw: 3 };
 
+const desiredOutcomeMap = {
+    X: "lose",
+    Y: "draw",
+    Z: "win",
+};
+
 function rockPaperScissors(you, them) {
     // paper beats rock
     // rock beats scissors
@@ -56,25 +70,63 @@ function rockPaperScissors(you, them) {
     }
     return null; // should be unreachable
 }
+
 const data = readData("./strategyGuide.txt")
     .split("\n")
     .filter((d) => !!d);
-let sum = 0;
-for (let game of data) {
-    game = game.split(" ");
 
-    const them = game[0];
-    const me = game[1];
+function playFullSheet(data) {
+    let sum = 0;
+    for (let game of data) {
+        game = game.split(" ");
 
-    const result = rockPaperScissors(
-        yourMove[me].action,
-        response[them].action
-    );
+        const them = game[0];
+        const me = game[1];
 
+        const result = rockPaperScissors(
+            yourMove[me].action,
+            response[them].action
+        );
 
-    sum += resultVal[result] + yourMove[me].score;
-    console.log(`me ${me}, them ${them}, result ${result}`);
-    // if(sum > 100) break;
+        sum += resultVal[result] + yourMove[me].score;
+        // console.log(`me ${me}, them ${them}, result ${result}`);
+    }
+    return sum;
 }
 
-console.log(`sum: ${sum}`);
+function win(them) {
+    if (them == actions.Paper) return actions.Scissors;
+    if (them == actions.Rock) return actions.Paper;
+    if (them == actions.Scissors) return actions.Rock;
+}
+function lose(them) {
+    if (them == actions.Rock) return actions.Scissors;
+    if (them == actions.Scissors) return actions.Paper;
+    if (them == actions.Paper) return actions.Rock;
+}
+
+function reachDesiredOutcome(data) {
+    let sum = 0;
+    for (let game of data) {
+        game = game.split(" ");
+        const them = game[0];
+        const desiredOutcome = game[1];
+        let myMove;
+        if (desiredOutcomeMap[desiredOutcome] === result.win)
+            myMove = win(response[them].action);
+        else if (desiredOutcomeMap[desiredOutcome] === result.lose)
+            myMove = lose(response[them].action);
+        else if (desiredOutcomeMap[desiredOutcome] === result.draw)
+            myMove = response[them].action;
+
+        // console.log(
+        //     `they played ${response[them].action}, desiredOutcome ${desiredOutcomeMap[desiredOutcome]}`
+        // );
+        // console.log(`my move ${myMove}  \n`);
+        sum += resultVal[desiredOutcomeMap[desiredOutcome]] + myVals[myMove];
+    }
+    return sum;
+}
+
+console.log(`part 1 ${playFullSheet(data)}`);
+console.log(`part 2 ${reachDesiredOutcome(data)}`)
