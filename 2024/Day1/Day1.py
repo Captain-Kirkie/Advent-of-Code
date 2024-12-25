fd6d8a69d6893e3177eb79c7112b05fa482ba031
KirkHietpas <hietpaskirk@gmail.com>
2024-12-25 16:05:00 -0700

    Part 2 day 1

--- 2024/Day1/Day1.py
+++ 2024/Day1/Day1.py
@@ -2,7 +2,7 @@
 def read_puzzle():
     arr1 = []
     arr2 = []
-    with open('./Day1/Day1Input.txt') as file:
+    with open('Day1Input.txt') as file:
         lines = file.readlines()
         for line in lines:
             split = line.split(" ")
@@ -12,16 +12,45 @@ def read_puzzle():
     return arr1, arr2
 
 
-if __name__ == "__main__":
-    result = read_puzzle()
-    arr1 = result[0]
-    arr2 = result[1]
+def part_1():
+    print("Part 1")
+    puzzle = read_puzzle()
+    arr1 = puzzle[0]
+    arr2 = puzzle[1]
     arr1.sort()
     arr2.sort()
-    print(f"arr1 {arr1}")
-    print(f"arr2 {arr2}")
     diffs = []
     for i in range(len(arr1)):
         diff = abs(arr1[i] - arr2[i])
         diffs.append(diff)
-    print(f"Answer: {sum(diffs)}")
+    print(f"Answer Part1: {sum(diffs)}")
+
+def part_2():
+    print("Part 2")
+    puzzle = read_puzzle()
+    arr1 = puzzle[0]
+    arr2 = puzzle[1]
+    arr1.sort()
+    arr2.sort()
+
+    count = {}
+    
+    for num in arr2:
+        if num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
+    
+    need_to_sum = []
+    
+    for num in arr1:
+        if num in count.keys():
+            value = num * count[num]
+            need_to_sum.append(value)
+        else:
+            need_to_sum.append(0)
+    print(f"Answer2: {sum(need_to_sum)}")
+
+if __name__ == "__main__":
+  part_1()
+  part_2()
